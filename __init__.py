1cda2cc2e3308f21352a89760e932af17aa61081
Jordi Esteve <jesteve@zikzakmedia.com>
2015-02-26 18:10:35 +0100

    Add receivable_lines/payable_lines functional fields in party (useful for other modules)

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
 # This file is part of account_move_line_related module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+from trytond.pool import Pool
+from .party import *
+
+
+def register():
+    Pool.register(
+        Party,
+        module='account_move_line_related', type_='model')
