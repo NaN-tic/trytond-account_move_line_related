a6e05bd2a06f5956b16b258e2ba56f27e272a5e9
jmartin <jmartin@zikzakmedia.com>
2016-03-29 11:43:13 +0200

    Add python 3 support

--- party.py
+++ party.py
@@ -5,10 +5,10 @@ from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 
 __all__ = ['Party']
-__metaclass__ = PoolMeta
 
 
 class Party:
+    __metaclass__ = PoolMeta
     __name__ = 'party.party'
 
     receivable_lines = fields.Function(fields.One2Many('account.move.line',
